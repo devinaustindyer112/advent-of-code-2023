0d246ab0ec0456ecc286855edce944cf4a1ce421
Devin Dyer <dyerda112@gmail.com>
2024-10-03 06:50:06 -0400

    read in new input file, loop through lines and print

--- main.go
+++ main.go
@@ -1,5 +1,22 @@
 package main
 
+import (
+	"bufio"
+	"log"
+	"os"
+)
+
 func main() {
 	print("start day 2")
+	
+	file, err := os.Open("./input.txt")
+	if err != nil {
+		log.Fatal("error opening file")
+	}
+
+	scanner := bufio.NewScanner(file)
+
+	for scanner.Scan() {
+		println(scanner.Text())
+	}
 }
