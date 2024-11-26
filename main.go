a689f35162e84f9beeda626c728fcce15f992297
Devin Dyer <devin.dyer@efwnow.com>
2024-11-26 09:04:21 -0500

    continue work on day 5

--- main.go
+++ main.go
@@ -4,19 +4,39 @@ import (
 	"fmt"
 	"os"
 	"regexp"
+	"strconv"
 	"strings"
 )
 
 type Node struct {
-	Value    string
-	Range    []string
-	Children []*Node
+	Type      string
+	ChildType string
+	Value     int
+	Range     []int
+	Children  []*Node
 }
 
-func (n *Node) add(child Node) {
-	if len(n.Children) == 0 {
-		n.Children = append(n.Children, &child)
+// I'm not sure this is the best solution, but I'm going to solve this using
+// a graph and nodes.
+
+func (n *Node) add(node *Node) {
+
+	// Root node
+	if n.Value < 0 {
+		n.Children = append(n.Children, node)
 	}
+
+	for _, child := range n.Children {
+		// Create function for checking if in range
+		if node.Type == child.ChildType {
+			child.Children = append(child.Children, node)
+		}
+		child.add(node)
+	}
+}
+
+func (n *Node) isCompatible(node *Node) {
+
 }
 
 func (n *Node) countChildren() int {
@@ -28,49 +48,38 @@ func main() {
 	day_5_part_1(string(input))
 }
 
-// This seems like a graph problem
-// Every entity maps only to a single entity. I don't believe we need to account for overlapping.
-
 func day_5_part_1(input string) {
-	// Parse seeds
-	// Parse soil
-	// Find lowest value for soil.
-	// Implement the rest of the solution
 
 	regex := regexp.MustCompile(`(?m)^\s*$`)
 	sections := regex.Split(input, -1)
 	assert(len(sections) == 8, fmt.Sprintf("Sections length incorrect: %d", len(sections)))
 
 	root := Node{
-		Value: "root",
+		Value: -1,
 	}
 
-	mapSeeds(&root, sections[0])
+	mapElement(&root, sections[0])
 	assert(root.countChildren() == 20, fmt.Sprintf("Children length incorrect: %d", root.countChildren()))
 }
 
-func mapSeeds(root *Node, input string) {
+func mapElement(root *Node, input string) {
 	inputSplit := strings.Split(input, ":")
 	assert(len(inputSplit) == 2, fmt.Sprintf("Split input length incorrect: %d", len(inputSplit)))
 
-	seeds := strings.Split(strings.Trim(inputSplit[1], " "), " ")
-	assert(len(seeds) == 20, fmt.Sprintf("Seeds length incorrect: %d", len(seeds)))
+	inputValues := strings.Split(strings.Trim(inputSplit[1], " "), " ")
+	assert(len(inputValues) == 20, fmt.Sprintf("Input values length incorrect: %d", len(inputValues)))
 
-	for _, seed := range seeds {
+	for _, inputValue := range inputValues {
+		elementValue, _ := strconv.Atoi(inputValue)
+		elementType := inputSplit[0]
+
 		child := Node{
-			Value: seed,
+			Type:  elementType,
+			Value: elementValue,
 		}
-		root.add(child)
+
+		root.add(&child)
 	}
-}
-
-func mapSoil(root *Node, input string) {
-	inputSplit := strings.Split(input, ":")
-	assert(len(inputSplit) == 2, fmt.Sprintf("Split input length incorrect: %d", len(inputSplit)))
-
-	regex := regexp.MustCompile(`\n$`)
-	soilMap := regex.Split(inputSplit[1], -1)
-
 }
 
 func assert(condition bool, errMessage string) {
