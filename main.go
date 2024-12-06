89584c475c2e9b6cfe44e3974e19708faad04c61
Devin Dyer <devin.dyer@efwnow.com>
2024-12-05 20:09:34 -0500

    more work on day 5. she slow

--- main.go
+++ main.go
@@ -20,27 +20,72 @@ func main() {
 }
 
 // For novel problems, niave solution first!
-// Understand the problem fully before trying to complete it
+// Understand the problem fully before trying to complete it!
 
 func day_5_part_1(input string) {
 	regex := regexp.MustCompile(`(?m)^\s*$`)
 	sections := regex.Split(input, -1)
 	assert(len(sections) == 8, fmt.Sprintf("Sections length incorrect: %d", len(sections)))
 
-	seeds := strings.Split(strings.Trim(strings.Split(sections[0], ":")[1], " "), " ")
+	seeds := regexp.MustCompile(`[0-9]+`).FindAllString(sections[0], -1)
 	assert(len(seeds) == 20, fmt.Sprintf("Seeds length incorrect: %d", len(seeds)))
 
-	parseMap(sections[1])
+	seedToSoilMap := parseMap(sections[1])
 
-	// Start with a single map and getting the appropriate values
+	soils := getSoils(seeds, seedToSoilMap)
+	assert(len(soils) >= 0, fmt.Sprintf("Invalid soils length %d", soils))
 
+	for _, soil := range soils {
+		println(soil)
+	}
+
+	// Start with a single map and getting the lowest value.
 }
 
-func parseMap(section string) {
-	entriesList := strings.Split(strings.Trim(strings.Split(strings.Trim(section, "\n"), ":")[1], "\n"), "\n")
-	assert(len(entriesList) == 23, fmt.Sprintf("Map values incorrect length %d", len(entriesList)))
+func getSoils(seeds []string, seedToSoilArray []MapEntry) []int {
 
+	var soils []int
+	for _, seed := range seeds {
+		soil := getSoil(seed, seedToSoilArray)
+		soils = append(soils, soil)
+	}
+
+	return soils
+}
+
+func getSoil(seed string, seedToSoilArray []MapEntry) int {
+
+	for _, seedToSoil := range seedToSoilArray {
+
+		if !isWithinRange(parseInt(seed), seedToSoil) {
+			continue
+		}
+
+		for i := 0; i < seedToSoil.RangeLength; i++ {
+			if seedToSoil.OriginStart+i == parseInt(seed) {
+				return seedToSoil.DestinationStart + i
+			}
+		}
+
+	}
+
+	return -1
+}
+
+func isWithinRange(seed int, mapEntry MapEntry) bool {
+
+	if mapEntry.OriginStart <= seed && seed <= mapEntry.OriginStart+mapEntry.RangeLength {
+		return true
+	}
+
+	return false
+}
+
+func parseMap(section string) []MapEntry {
+	entriesList := strings.Split(strings.Trim(strings.Split(strings.Trim(section, "\n"), ":")[1], "\n"), "\n")
+	assert(len(entriesList) == 23, fmt.Sprintf("Map entries incorrect length %d", len(entriesList)))
 	var entriesMap []MapEntry
+
 	for _, entry := range entriesList {
 		entryValues := strings.Split(entry, " ")
 		entriesMap = append(entriesMap, MapEntry{
@@ -49,18 +94,25 @@ func parseMap(section string) {
 			RangeLength:      parseInt(entryValues[2]),
 		})
 	}
+	assert(len(entriesMap) == 23, fmt.Sprintf("Map entry slice incorrect length %d", len(entriesMap)))
+
+	return entriesMap
 }
 
 func parseInt(str string) int {
 	value, err := strconv.Atoi(str)
+
 	if err != nil {
-		panic("error parsing string to int")
+		panic(fmt.Sprintf("Error parsing string to int %s", err.Error()))
 	}
+
 	return value
 }
 
 func assert(condition bool, errMessage string) {
+
 	if !condition {
 		panic(errMessage)
 	}
+
 }
